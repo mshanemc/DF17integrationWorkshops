24d937d76dc4183a294cef577b1f720be692c654
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-11-01 12:26:03 -0500

    set default

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1
+sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx force:source:push
 sfdx force:user:permset:assign -n CustomerID
 sfdx force:user:password:generate
