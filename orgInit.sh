09bcdbbe5badbc941d4b4f28003662b97480f115
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-12-29 12:44:35 -0600

    friendlier username

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx msm:org:create -f config/project-scratch-def.json -d 1 -s --json -u int -o work.shop
 sfdx force:source:push
 sfdx force:user:permset:assign -n CustomerID
 sfdx msm:user:password:set -l User -g User -p sfdx1234 --json
