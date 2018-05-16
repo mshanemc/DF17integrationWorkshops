80ef214ef34c699ebcff6c2acd7261a60c5c0ec6
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-05-16 08:22:59 -0500

    remove yaml

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,6 @@
-sfdx msm:org:create -f config/project-scratch-def.json -d 1 -s --json -u int -o work.shop
+sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s --json -u int -o work.shop
 sfdx force:source:push
 sfdx force:user:permset:assign -n CustomerID
-sfdx msm:user:password:set -l User -g User -p sfdx1234 --json
+sfdx shane:user:password:set -l User -g User -p sfdx1234 --json
 sfdx force:apex:execute -f scripts/CustomerIDSetup.cls
 sfdx force:org:open
